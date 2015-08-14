6589c8c220ad6498acd67ed3cc33841a0aa159f7
Haibin Wang <hbwang85@hotmail.com>
2015-08-14 16:30:09 +0800

    fix error

--- microblog/app.js
+++ microblog/app.js
@@ -41,7 +41,7 @@ if (app.get('env') === 'development') {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      //error: err
+      error: err
     });
   });
 }
