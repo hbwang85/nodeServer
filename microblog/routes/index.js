44236a509ab307d0d4b9a3583f3ebf0d0db4eb61
Haibin Wang <hbwang85@hotmail.com>
2015-08-14 17:18:44 +0800

    add router to resolve /user/[username]

--- microblog/routes/index.js
+++ microblog/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//router.get('/users', function(req, res) {
+//  res.send('JUST FOR YOU');
+//});
+
 module.exports = router;
